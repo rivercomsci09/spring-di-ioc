3395cb5e79af66643b18ec2fa635f89c264e4ebd
Tran Truong Giang <rivercomsci09@gmail.com>
2020-04-23 15:29:21 +0700

    Annotaion @Value

--- src/main/java/com/river/demo/ApplicationConfig.java
+++ src/main/java/com/river/demo/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.river.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
     @Bean
     SimpleBean simpleBeanConfigure(){
-        return new SimpleBean("RIVER");
+        return new SimpleBean("RIVER" + river);
     }
+
+    @Value("${river.value}")
+    String river;
 }
