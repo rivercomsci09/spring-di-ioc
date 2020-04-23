47a5bbde35ffc7f7d16310f7b6d864e2a3bc05bb
Tran Truong Giang <rivercomsci09@gmail.com>
2020-04-23 14:47:52 +0700

    ectory

--- src/main/java/com/river/demo/DemoApplication.java
+++ src/main/java/com/river/demo/DemoApplication.java
@@ -3,13 +3,16 @@ package com.river.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
+		System.out.println("After IoC container init");
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Before IoC container init");
 
 //		Outfit outfit = new Bikini();
 		/**
@@ -37,5 +40,18 @@ public class DemoApplication {
 
 		girl.getOutfit().wear();
 
+		/**
+		 * Bean Life Cycle:
+		 * IoC container = ApplicationContext find Bean and init by constructor
+		 * inject dependencies Bean by Setter
+		 * Method mark @PostConstruct will be call
+		 * Some handling after @PostConstruct
+		 * Bean ready
+		 * after @PreDestroy
+		 * Delete Bean
+		 */
+//		System.out.println("After IoC Container destroy Girl");
+//		((ConfigurableApplicationContext) context).getBeanFactory().destroyBean(girl);
+//		System.out.println("Before IoC Container destroy Girl");
 	}
 }
--- src/main/java/com/river/demo/Girl.java
+++ src/main/java/com/river/demo/Girl.java
@@ -3,11 +3,24 @@ package com.river.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class Girl {
 
     @Autowired
     private Outfit outfit;
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Instance of Girl AFTER initialization will run this method");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("Instance of Girl BEFOR destroy will run this method");
+    }
 
     public Girl(Outfit outfit) {
         this.outfit = outfit;
